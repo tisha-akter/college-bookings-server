9bcbaa1d3ba6c664f533dc9e34a799541623e69e
tisha-akter <tishaakter.jui79@gmail.com>
2023-07-24 18:19:34 +0600

    .

--- index.js
+++ index.js
@@ -38,14 +38,19 @@ async function run() {
     });
 
 
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post('/users', async (req, res) => {
       const user = req.body;
-      // const query = { email: user?.email }
-      // const existingUser = await usersCollection.findOne(query);
+      const query = { email: user?.email }
+      const existingUser = await usersCollection.findOne(query);
 
-      // if (existingUser) {
-      //   return res.send({ message: 'user already exists' })
-      // }
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
